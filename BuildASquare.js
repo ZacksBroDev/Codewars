c25ff1036b67194907955a4f305ef6a123e4bfad
Zack Brown <zackaryzbrown@gmail.com>
2025-11-21 23:32:16 -0800

    built a square out of Plus signs (+) and returns the resulting string. 7pts

---
+++ BuildASquare.js
@@ -0,0 +1,11 @@
+function generateShape(integer) {
+  let result = "";
+  for (let i = 0; i < integer; i++) {
+    result += "+".repeat(integer);
+    if (i < integer - 1) {
+      result += "\n";
+    }
+  }
+
+  return result;
+}
