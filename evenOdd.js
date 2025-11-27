c4d7f69bd4f9d9250004b700a54d54e6e0bb7a6e
Zack Brown <zackaryzbrown@gmail.com>
2025-11-26 21:02:10 -0700

    Learned and improved

--- evenOdd.js
+++ evenOdd.js
@@ -1,11 +1,14 @@
 function evenOrOdd(number) {
-  if(number %2){
+  if(number % 2){ // Add !== 0 for clarity
     return "Odd"
   }
   return "Even"
 }
 
+// Tests
 console.log(evenOrOdd(2))
 console.log(evenOrOdd(3))
 console.log(evenOrOdd(-42))
 console.log(evenOrOdd(0));
+
+//   return number % 2 === 0 ? "Even" : "Odd"; would be better and shorter
