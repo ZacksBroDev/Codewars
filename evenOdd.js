309780884f9bffc1f77c9d176ce7dc4e0e5ea81b
Zack Brown <zackaryzbrown@gmail.com>
2025-11-26 20:55:18 -0700

    Even or Odd Code Wars training

---
+++ evenOdd.js
@@ -0,0 +1,11 @@
+function evenOrOdd(number) {
+  if(number %2){
+    return "Odd"
+  }
+  return "Even"
+}
+
+console.log(evenOrOdd(2))
+console.log(evenOrOdd(3))
+console.log(evenOrOdd(-42))
+console.log(evenOrOdd(0));
