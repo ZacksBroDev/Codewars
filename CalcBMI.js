cef71675c33f2ab1873b1cb4520fd96f48167989
Zack Brown <zackaryzbrown@gmail.com>
2026-01-12 22:17:29 -0700

    Calculate BMI codewars

---
+++ CalcBMI.js
@@ -0,0 +1,15 @@
+function bmi(weight, height) {
+  if (height <= 0) {
+    return "Invalid height";
+  }
+  const bmiValue = weight / (height * height);
+  if (bmiValue <= 18.5) {
+    return "Underweight";
+  } else if (bmiValue <= 25) {
+    return "Normal";
+  } else if (bmiValue <= 30) {
+    return "Overweight";
+  } else {
+    return "Obese";
+  }
+}
