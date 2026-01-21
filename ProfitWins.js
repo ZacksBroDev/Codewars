74df021c3977e7bb2a2722ef7976a1f5f1856613
Zack Brown <zackaryzbrown@gmail.com>
2026-01-20 22:42:51 -0700

    Profit Wins codewars

---
+++ ProfitWins.js
@@ -0,0 +1,17 @@
+function minMax(arr) {
+  // return [Math.min(...arr), Math.max(...arr)];
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return [min, max];
+}
+
+console.log(minMax([1, 2, 3, 4, 5])); // [1, 5]
