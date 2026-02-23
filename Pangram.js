1b5e8f1a5394f1aa7daa64bf22551c982b71dc37
Zack Brown <zackaryzbrown@gmail.com>
2026-02-22 18:28:20 -0800

    Detect Pangram code wars

---
+++ Pangram.js
@@ -0,0 +1,12 @@
+function isPangram(string) {
+  const seen = new Set();
+  for (const char of string.toLowerCase()) {
+    if (char >= "a" && char <= "z") {
+      seen.add(char);
+      if (seen.size === 26) {
+        return true;
+      }
+    }
+  }
+  return seen.size === 26;
+}
